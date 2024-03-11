ce3540d8f90b06d69a3973db5a9e1a75c427ba89
aaronwalker7 <aaronwalker7>
2024-03-11 16:42:55 +0000

    changed everything into classes for reproducability, encapsulating, readability

--- AAPL_stock_predictions.py
+++ AAPL_stock_predictions.py
@@ -44,7 +44,7 @@ spark.sql(f"CREATE DATABASE IF NOT EXISTS aaronxwalker")
 
 # COMMAND ----------
 
-dbutils.library.restartPython()
+# dbutils.library.restartPython()
 
 # COMMAND ----------
 
@@ -56,6 +56,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from pyspark import pandas as ps
 import datetime
+from fredapi import Fred
+
+# COMMAND ----------
+
+fred = Fred(api_key_file='fred_api_key.txt')
 
 # COMMAND ----------
 
@@ -95,9 +100,92 @@ import datetime
 
 # COMMAND ----------
 
+class StockData(pd.DataFrame):
+    def __init__(self, ticker, start):
+        """
+        Initialize StockData object.
+
+        Parameters:
+        - ticker (str): Ticker symbol of the stock.
+        - start (str): Start date for stock data retrieval (format: 'YYYY-MM-DD').
+        """
+        # Fetch stock data using yfinance
+        stock_data = yf.Ticker(ticker).history(start=start)
+        
+        # Call the superclass (DataFrame) constructor
+        super().__init__(stock_data)
+
+    def attach_fred_data(self, tickers):
+        """
+        Attach FRED data to the StockData object.
+
+        Parameters:
+        - fred_tickers (list): List of FRED data series tickers.
+        """
+        for data in tickers:
+            df = fred.get_series_all_releases(data, realtime_start=self.index.min().strftime('%Y-%m-%d'), realtime_end=datetime.datetime.now().strftime('%Y-%m-%d'))
+            df['realtime_start'] = df['realtime_start'].dt.tz_localize('America/New_York')
+            df.drop_duplicates(subset='realtime_start', keep='last', inplace=True)
+            for col in renamed_cols:
+                df.rename(columns={col: col + '_' + data}, inplace=True)
+            self = self.merge(right=df, left_index=True, right_on='realtime_start', how='left')
+            self.set_index('realtime_start', inplace=True, drop=True)
+        return self
+
+
+# COMMAND ----------
+
+ticker = 'AAPL'
+start_date = '2017-01-01'
+
+# Create an instance of StockData
+AAPL = StockData(ticker, start_date)
+
+# COMMAND ----------
+
+stock_data_instance
+
+# COMMAND ----------
+
+# Attach FRED data
+stock_data_instance = stock_data_instance.attach_fred_data(fred_data)
+
+# COMMAND ----------
+
+# Now, stock_data_instance behaves like a DataFrame with additional functionalities
+print(stock_data_instance.head())
+
+# COMMAND ----------
+
+class stockData:
+    def __init__(self, ticker, start):
+        self.start = start
+        self.df = yf.Ticker(ticker).history(start = start)
+
+    def attach_fred_data(self, tickers):
+        self.bigger_data = self.df
+        for data in tickers:
+            df = fred.get_series_all_releases(data, realtime_start=self.start, realtime_end=datetime.datetime.now().strftime('%Y-%m-%d'))
+            df['realtime_start'] = df['realtime_start'].dt.tz_localize('America/New_York')
+            df.drop_duplicates(subset = 'realtime_start', keep = 'last', inplace = True)
+            for col in renamed_cols:
+                df.rename(columns={col:col+'_'+data}, inplace = True)
+            self.bigger_data = self.bigger_data.merge(right = df, left_index= True, right_on= 'realtime_start', how = 'left')
+            self.bigger_data.set_index('realtime_start', inplace = True, drop = True)
+
+# COMMAND ----------
+
+StockAAPL = stockData('AAPL', '2017-01-01')
+APPL = StockAAPL.df
+StockAAPL.attach_fred_data(fred_data)
+AAPL_bigger = StockAAPL.bigger_data
+StockAAPL
+
+# COMMAND ----------
+
 stock = 'AAPL'
 aapl = yf.Ticker(stock)
-AAPL = aapl.history(start = '2015-01-01')
+AAPL = aapl.history(start = '2017-01-01')
 
 # COMMAND ----------
 
@@ -111,16 +199,15 @@ AAPL = aapl.history(start = '2015-01-01')
 
 # COMMAND ----------
 
-from fredapi import Fred
-fred = Fred(api_key_file='fred_api_key.txt')
-
-# COMMAND ----------
-
 # MAGIC %md
 # MAGIC
 # MAGIC #### the following are the tickers for the macro datasets and their name
 # MAGIC
 # MAGIC T10YIE - 10 year average expected inflation in US
+# MAGIC
+# MAGIC MORTGAGE30US - 30 year mortgage rate in US
+# MAGIC
+# MAGIC MORTGAGE15US - 15 year mortgage rate in US
 # MAGIC
 # MAGIC CPIAUCSL - inflation rate in US
 # MAGIC
@@ -133,25 +220,56 @@ fred = Fred(api_key_file='fred_api_key.txt')
 # MAGIC FEDFUNDS - effective federal funds rate (overnight interbank borrowing) in US
 # MAGIC
 # MAGIC REAINTRATREARAT10Y - 10 year real interest rate in US (nominal - inflation)
+# MAGIC
+# MAGIC A792RC0Q052SBEA - personal income per capita in US
+# MAGIC
+# MAGIC POPTHM - population in US
+# MAGIC
+# MAGIC GBPUSD=X - GBP to USD conversion rate
+# MAGIC
+# MAGIC EURUSD=X - EUR to USD conversion rate
+# MAGIC
+# MAGIC CNY=X - CNY to USD conversion rate
+# MAGIC
+# MAGIC
 
 # COMMAND ----------
 
-macroeconomic = ['T10YIE', 'CPIAUCSL', 'UNRATE', 'GDP', 'GDPC1', 'FEDFUNDS', 'REAINTRATREARAT10Y']
+fred_data = ['T10YIE', 'CPIAUCSL', 'UNRATE', 'GDP', 'GDPC1', 'FEDFUNDS', 'REAINTRATREARAT10Y', 'MORTGAGE30US', 'MORTGAGE15US', 'A792RC0Q052SBEA']
+yahoo_data = ['GBPUSD=X', 'EURUSD=X', 'CNY=X']
+renamed_cols = fred.get_series_all_releases('T10YIE', realtime_start='2017-01-01', realtime_end=datetime.datetime.now().strftime('%Y-%m-%d')).columns.drop('realtime_start')
 
 # COMMAND ----------
 
-data = fred.get_series('A939RX0Q048SBEA')
-data
+for data in fred_data:
+    df = fred.get_series_all_releases(data, realtime_start='2017-01-01', realtime_end=datetime.datetime.now().strftime('%Y-%m-%d'))
+    df['realtime_start'] = df['realtime_start'].dt.tz_localize('America/New_York')
+    df.drop_duplicates(subset = 'realtime_start', keep = 'last', inplace = True)
+    for col in renamed_cols:
+        df.rename(columns={col:col+'_'+data}, inplace = True)
+    AAPL = AAPL.merge(right = df, left_index= True, right_on= 'realtime_start', how = 'left', suffixes = None)
+    AAPL.set_index('realtime_start', inplace = True, drop = True)
+    print(f'done {data}')
+
+# COMMAND ----------
+
+AAPL
+
+# COMMAND ----------
+
+df = fred.get_series_all_releases('T10YIE', realtime_start='2017-01-01', realtime_end=datetime.datetime.now().strftime('%Y-%m-%d'))
+df['realtime_start'] = df['realtime_start'].dt.tz_localize('America/New_York')
+df.drop_duplicates(subset = 'realtime_start', keep = 'last', inplace = True)
+
+
+# COMMAND ----------
+
+AAPL.merge(df, left_index= True, right_on='realtime_start', how = 'left', suffixes = ('x','y'))
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC get realtime_start to see when it was actually recorded so you know how long to lag it by, for example in the below it has a date of 10th October 2023 for the data (beginning of the quarter) but actually it wasn't available data until 28th February 2024
-
-# COMMAND ----------
-
-df = fred.get_series_all_releases('A939RX0Q048SBEA')
-df.tail()
 
 # COMMAND ----------
 
